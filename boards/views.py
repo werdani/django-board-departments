2e869cad1fe825c2e18e12257318d4c51601a81c
werdani <https://github.com/werdani/django-job-board.git>
2020-10-09 19:01:36 +0200

    create a view from Board

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Board
 # Create your views here.
+
+
+def home(request):
+    boards = Board.objects.all()
+    board_name = []
+    for board in boards:
+        board_name.append(board.name)
+    html = '<br>'.join(board_name)
+    return HttpResponse(html)
+
