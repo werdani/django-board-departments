636f4c500f3f774b4a999d418984ad1d4ed75e23
werdani <https://github.com/werdani/django-job-board.git>
2020-10-23 22:30:57 +0200

    create update profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('settings/change_password/',auth_views.PasswordChangeView.as_view(template_name='change_password.html'),name='change_password'),
     path('settings/change_password/done/',auth_views.PasswordChangeDoneView.as_view(template_name='change_done.html'),name='password_change_done'),
-
+    path('account/',views.UserUpdateView.as_view(),name='my_account'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.views.generic import UpdateView
 from .forms import SignUpForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 # Create your views here.
 def signup(request):
@@ -13,3 +16,13 @@ def signup(request):
             login(request,user)
             return redirect('home')
     return render(request,'signup.html',{'form':form})
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name','last_name','email',)
+    template_name ='my_account.html'
+    success_url = reverse_lazy('my_account')
+
+    def get_object(self):
+        return self.request.user
